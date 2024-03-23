95f3352bf2e77b982f53546da8d7d5930f1ade59
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2024-03-23 16:55:36 -0400

    Refactor test suite commands

--- src/tests.js
+++ src/tests.js
@@ -1,61 +1,108 @@
 var colour, submit, prompt;
 
-function Tests(_colour, _submit, _display) => {
+function Tests(_colour, _submit, _prompt) {
 	colour = _colour;
 	submit = _submit;
-	prompt = _display.prompt;
+	prompt = _prompt;
+	return this;
 };
 
-const firstPoll = {
-	step: () => {
+const show = {
+	intro: () => {
+		submit('',[
+			'tests.show.bag(bag);  - lists all tiddlers in a bag',
+			].join('\n')
+		);
+	},
+	bag: (bag) => submit(`$tw.mws.store.getBagTiddlers('${bag}')\n`, prompt),
+}
+
+
+const addTiddler = {
+	intro: () => {
+		submit(`.clear\n`);
+		submit(`tests.addTiddler.step(1)`,[
+			'\n----- Test adding new tiddler -----',
+			'There are 5 steps in this test',
+			' - Step #1 returns the top 20 tiddlers in bag-alpha',
+			' - Step #2 returns no tiddlers - step #1 already got them',
+			' - Step #3 adds a tiddler to bag-alpha',
+			' - Step #4 returns tiddler added',
+			' - Step #5 returns no tiddlers - step #4 already got the added tiddler',
+			'\nStep 1 returns the top 20 tiddlers in bag-alpha'
+			].join('\n')
+		);
+	},
+
+	step: (nbr) => {
 		if (nbr === 1) {
-			colour.log(`\n----- poll #1 -----\n`,10);
-			colour.log(`Specify only the bag name param to get list of most recent tiddlers\n`,10);
-			colour.log(` up to the 'limit' - currently 20 tiddlers`,10);
-			prompt();
-			submit.cmd(`var poll_1 = $tw.mws.store.getBagRecentTiddlers('bag-alpha')\n`);
-			submit.cmd(`showObj(poll_1)\n`);
-			prompt();
-			submit.cmd(`step(2) // Poll again using bag_max_tiddler_id from previous poll (#1)`);
+			submit(`var poll_1 = $tw.mws.store.getBagRecentTiddlers('bag-alpha')\n`,[
+				'\n----- Step #1 -----',
+				'Specify only the bag name param to get list of most recent tiddlers',
+				` up to the 'limit' - currently 20 tiddlers`,
+				].join('\n')
+			);
+			submit(`showObj(poll_1)\n`);
+
+			submit(`tests.addTiddler.step(2)`,
+				`\nStep 2 will return no tiddlers - step #1 already got them`);
 		}
+
 		else if (nbr === 2) {
-			colour.log(`\n----- poll #2 -----\n`,10);
-			colour.log(`Using the bag_max_tiddler_id from poll #1\n`,10);
-			colour.log(`Returns no tiddlers as tiddler_id needs to be greater\n`,10);
-			colour.log(` than poll #1's bag_max_tiddler_id`,10);
-			prompt();
-			submit.cmd(`poll_1.bag_max_tiddler_id\n`);
-			submit.cmd(`var poll_2 = $tw.mws.store.getBagRecentTiddlers('bag-alpha',poll_1.bag_max_tiddler_id)\n`);
-			submit.cmd(`showObj(poll_2)\n`);
-			prompt();
-			submit.cmd(`step(3) // Add a tiddler to bag-alpha`);
+			submit(`poll_1.bag_max_tiddler_id\n`,[
+				'\n----- Step #2 -----',
+				'Using the bag_max_tiddler_id from step #1',
+				`Returns no tiddlers as tiddler_id needs to be greater than step #1's bag_max_tiddler_id`
+				].join('\n')
+			);
+			submit(`var poll_2 = $tw.mws.store.getBagRecentTiddlers('bag-alpha', poll_1.bag_max_tiddler_id)\n`);
+			submit(`showObj(poll_2)\n`);
+
+			submit(`tests.addTiddler.step(3)`,'\nStep #3 adds a tiddler to bag-alpha');
 		}
+
 		else if (nbr === 3) {
-			colour.log(`\n---- Add tiddler -----`,10);
-			prompt();
-			submit.cmd(`var newTiddler = {\n`);
-			submit.cmd(`title: 'In the bag',\n`);
-			submit.cmd(`text: 'This tiddler is in bag-alpha'\n`);
-			submit.cmd(`}\n`);
-			submit.cmd(`$tw.mws.store.saveBagTiddler(newTiddler,'bag-alpha')\n`);
-			prompt();
-			submit.cmd(`step(4) // Poll again using bag_max_tiddler_id from previous poll (#2)`);
+			submit(`var newTiddler = {\n`,'\n---- Step #3 Add tiddler to bag-alpha -----');
+			submit(`title: 'In the bag',\n`);
+			submit(`text: 'This tiddler is in bag-alpha'\n`);
+			submit(`}\n`);
+			submit(`$tw.mws.store.saveBagTiddler(newTiddler,'bag-alpha')\n`);
+
+			submit(`tests.addTiddler.step(4)`,`\nStep 4 returns tiddler added`);
 		}
+
 		else if (nbr === 4) {
-			colour.log(`\n----- poll #3 -----\n`,10);
-			colour.log(`Using the bag_max_tiddler_id from poll #2\n`,10);
-			colour.log(`Returns new tiddler just added`,10);
-			prompt();
-			submit.cmd(`poll_2.bag_max_tiddler_id\n`);
-			submit.cmd(`var poll_3 = $tw.mws.store.getBagRecentTiddlers('bag-alpha',poll_2.bag_max_tiddler_id)\n`);
-			submit.cmd(`showObj(poll_3)\n`);
-			colour.log(`End of demo`,10);
-			prompt();
+			submit(`poll_2.bag_max_tiddler_id\n`,[
+				'\n----- Step #4 -----',
+				'Using the bag_max_tiddler_id from step #2',
+				'Returns new tiddler just added'
+				].join('\n')
+			);
+			submit(`var poll_3 = $tw.mws.store.getBagRecentTiddlers('bag-alpha', poll_2.bag_max_tiddler_id)\n`);
+			submit(`showObj(poll_3)\n`);
+
+			submit(`tests.addTiddler.step(5)`,'\nStep 5 returns no tiddlers- step #4 already got added tiddler');
 		}
+
+		else if (nbr === 5) {
+			submit(`poll_3.bag_max_tiddler_id\n`,[
+				'\n----- Step #5 -----',
+				'Using the bag_max_tiddler_id from step #4',
+				'Returns no tiddlers as step #4 already got added tiddler'
+				].join('\n')
+			);
+			submit(`var poll_4 = $tw.mws.store.getBagRecentTiddlers('bag-alpha', poll_3.bag_max_tiddler_id)\n`);
+			submit(`showObj(poll_4)\n`);
+
+			submit('', `\nEnd of test.addTiddler`);
+		}
+
+
 	}
 }
 
 module.exports = {
 	Tests: Tests,
-	firstPoll: firstPoll
+	addTiddler: addTiddler,
+	show: show,
 }
--- twrepl.js
+++ twrepl.js
@@ -22,162 +22,146 @@ const editionFolder = 'mws';
 const serverCommand = ['--listen','port=9200','host=127.0.0.1'];
 
 // -------------------
-// REPL display
-const submit = { cmd: (txt) => runtime._ttyWrite(txt) }
+// REPL interface
 const colour = {
-  log: (txt='', fg=255, bg=0, efg=255, ebg=0) => process.stdout.write(
-    `\x1b[38;5;${fg};48;5;${bg}m${txt}\x1b[38;5;${efg};48;5;${ebg}m`),
+	log: (txt='', fg=255, bg=0, efg=255, ebg=0) => process.stdout.write(
+		`\x1b[38;5;${fg};48;5;${bg}m${txt}\x1b[38;5;${efg};48;5;${ebg}m`),
 
-  txt: (txt='', fg=255, bg=0, efg=255, ebg=0) =>
-    `\x1b[38;5;${fg};48;5;${bg}m${txt}\x1b[38;5;${efg};48;5;${ebg}m`,
+	txt: (txt='', fg=255, bg=0, efg=255, ebg=0) =>
+		`\x1b[38;5;${fg};48;5;${bg}m${txt}\x1b[38;5;${efg};48;5;${ebg}m`,
 }
 const prompt = colour.txt('$mws-repl> ',33,0,7,0);
-const display = { prompt: () => colour.log(`\n\n${prompt}`,10) }
+const submit = (cmd, desc) => {
+	if (desc) {
+		if (desc === prompt) {
+			colour.log(`${prompt}`);
+		} else {
+			colour.log(desc, 10);
+			colour.log(`\n${prompt}`);
+		}
+	}
+	if (cmd) {
+		runtime._ttyWrite(cmd);
+	}
+}
 
-const tests = require('./src/tests').Tests(colour, submit, display);
+// Show objects 5 levels deep
+function showObj(obj) {
+	console.dir(obj, {depth:5});
+}
+
+// Test suite
+const tests = require('./src/tests').Tests(colour, submit, prompt);
+
+// -------------------
+// Remove database.sqlite
+const { unlink } = require('node:fs/promises');
+async function removeStoreDb() {
+  try {
+	const path = './mws/store/database.sqlite';
+    await unlink(path);
+    colour.log(`Successfully deleted ${path}\n\n`,130,0,110);
+  } catch (error) {
+	  if (error.errno !== -2) {
+		console.dir(error);
+	}
+  }
+}
 
 // -------------------
 // Who are we?
 const pkg = require('./package.json');
 function intro() {
-  colour.log( `${pkg.name}: `,75); colour.log(`v${pkg.version}\n\n`,130,0,110);
+	colour.log( `${pkg.name}: `,75); colour.log(`v${pkg.version}\n\n`,130,0,110);
 }
 
 // -------------------
 // TiddlyWiki commander
 // commander got an error?
 function checkForErrors(err) {
-    if (err) {
-        try {
-            $tw.utils.error("Error: " + err);
-        } catch (e) {}
-    }
+	if (err) {
+			try {
+					$tw.utils.error("Error: " + err);
+			} catch (e) {}
+	}
 }
 
 // Commander logs to console
 const log = {
-    write: (text) => console.log(text.toString())
+	write: (text) => console.log(text.toString())
 }
 
 // Create $tw.Commander to do... commands
 const cmdr = {
-    execute: (cmds) => {
-        colour.log('$tw.Commander: ',75); colour.log(JSON.stringify(cmds) + '\n',130,0,110);
-        new $tw.Commander(cmds, checkForErrors, $tw.wiki).execute();
-    }
+	execute: (cmds) => {
+			colour.log('$tw.Commander: ',75); colour.log(JSON.stringify(cmds) + '\n',130,0,110);
+			new $tw.Commander(cmds, checkForErrors, $tw.wiki).execute();
+	}
 }
-
-// -------------------
-// Initialize
-// Show our name and version
-intro();
-
-// Boot tiddlywiki module
-colour.log('Boot TiddlyWiki (multi-wiki-support)\n', 75);
-colour.log(`${pkg.dependencies.tiddlywiki}\n`,130,0,110);
-colour.log('-------------------\n',75);
-
-const $tw = require('tiddlywiki').TiddlyWiki();
-$tw.preloadTiddlers = require('./preloadTiddlers.json');
-$tw.boot.argv = [editionFolder];
-$tw.boot.boot(() => {});
-
-colour.log('-------------------\n\n', 75);
 
 // -------------------
 // Node'js REPL
 // Place $tw in REPL context so can be referenced
 function resetContext() {
-  runtime.context.$tw = $tw;
-  runtime.context.test = test;
+	runtime.context.$tw = $tw;
+	runtime.context.showObj = showObj;
+	runtime.context.tests = tests;
 }
 
 // REPL runtime
 var runtime;
 function startRepl() {
-  runtime = require('node:repl').start({
-      prompt: prompt, useColors: true,
-      ignoreUndefined: true, /*completer: completer*/
-  });
+	runtime = require('node:repl').start({
+			prompt: prompt, useColors: true,
+			ignoreUndefined: true, /*completer: completer*/
+	});
 
-  // If REPL is reset (.clear) - context needs resetting
-  runtime.on('reset', () => resetContext());
+	// If REPL is reset (.clear) - context needs resetting
+	runtime.on('reset', () => resetContext());
 
-  // Initial context settings
-  resetContext();
+	// Initial context settings
+	resetContext();
 }
 
 // -------------------
 // -------------------
 // Startup
+const $tw = require('tiddlywiki').TiddlyWiki();
+function startup () {
+	// -------------------
+	// Initialize
+	// Boot tiddlywiki module
+	colour.log('Boot TiddlyWiki (multi-wiki-support)\n', 75);
+	colour.log(`${pkg.dependencies.tiddlywiki}\n`,130,0,110);
+	colour.log('-------------------\n',75);
 
-// Fire up the Multi Wiki Server
-colour.log('Startup Multi Wiki Server\n', 75);
-colour.log('-------------------\n',75);
-cmdr.execute(serverCommand);
+	$tw.preloadTiddlers = require('./preloadTiddlers.json');
+	$tw.boot.argv = [editionFolder];
+	$tw.boot.boot(() => {});
 
-// Show prompt when startup done
-setTimeout(() => {
-  colour.log('-------------------\n\n',75);
-  startRepl();
-  submit.cmd(`// Show objects 5 levels deep\n`);
-  submit.cmd(`function showObj(obj) { console.dir(obj, {depth:5}) }\n`);
-  display.prompt();
-  submit.cmd('$tw.version\n');
-  display.prompt();
-  submit.cmd(`$tw.mws.store.listBags().map(bag => bag.bag_name)\n`);
-  display.prompt();
-  submit.cmd(`$tw.mws.store.getBagTiddlers('bag-alpha')\n`);
-  display.prompt();
-  submit.cmd(`step(1) // Start demo - first poll returns all (up to limit)`);
-}, 2000);
+	colour.log('-------------------\n\n', 75);
 
-/*
-function step(nbr) {
-  if (nbr === 1) {
-    colour.log(`\n----- poll #1 -----\n`,10);
-    colour.log(`Specify only the bag name param to get list of most recent tiddlers\n`,10);
-    colour.log(` up to the 'limit' - currently 20 tiddlers`,10);
-    display.prompt();
-    submit.cmd(`var poll_1 = $tw.mws.store.getBagRecentTiddlers('bag-alpha')\n`);
-    submit.cmd(`showObj(poll_1)\n`);
-    display.prompt();
-    submit.cmd(`step(2) // Poll again using bag_max_tiddler_id from previous poll (#1)`);
-  }
-  else if (nbr === 2) {
-    colour.log(`\n----- poll #2 -----\n`,10);
-    colour.log(`Using the bag_max_tiddler_id from poll #1\n`,10);
-    colour.log(`Returns no tiddlers as tiddler_id needs to be greater\n`,10);
-    colour.log(` than poll #1's bag_max_tiddler_id`,10);
-    display.prompt();
-    submit.cmd(`poll_1.bag_max_tiddler_id\n`);
-    submit.cmd(`var poll_2 = $tw.mws.store.getBagRecentTiddlers('bag-alpha',poll_1.bag_max_tiddler_id)\n`);
-    submit.cmd(`showObj(poll_2)\n`);
-    display.prompt();
-    submit.cmd(`step(3) // Add a tiddler to bag-alpha`);
-  }
-  else if (nbr === 3) {
-    colour.log(`\n---- Add tiddler -----`,10);
-    display.prompt();
-    submit.cmd(`var newTiddler = {\n`);
-    submit.cmd(`title: 'In the bag',\n`);
-    submit.cmd(`text: 'This tiddler is in bag-alpha'\n`);
-    submit.cmd(`}\n`);
-    submit.cmd(`$tw.mws.store.saveBagTiddler(newTiddler,'bag-alpha')\n`);
-    display.prompt();
-    submit.cmd(`step(4) // Poll again using bag_max_tiddler_id from previous poll (#2)`);
-  }
-  else if (nbr === 4) {
-    colour.log(`\n----- poll #3 -----\n`,10);
-    colour.log(`Using the bag_max_tiddler_id from poll #2\n`,10);
-    colour.log(`Returns new tiddler just added`,10);
-    display.prompt();
-    submit.cmd(`poll_2.bag_max_tiddler_id\n`);
-    submit.cmd(`var poll_3 = $tw.mws.store.getBagRecentTiddlers('bag-alpha',poll_2.bag_max_tiddler_id)\n`);
-    submit.cmd(`showObj(poll_3)\n`);
-    colour.log(`End of demo`,10);
-    display.prompt();
-  }
+
+	// Fire up the Multi Wiki Server
+	colour.log('Startup Multi Wiki Server\n', 75);
+	colour.log('-------------------\n',75);
+	cmdr.execute(serverCommand);
+
+	// Show prompt when startup done
+	setTimeout(() => {
+		colour.log('-------------------\n\n',75);
+		colour.log('Startup REPL\n', 75);
+		colour.log('-------------------\n\n',75);
+		startRepl();
+		submit('$tw.version\n');
+		submit(`$tw.mws.store.listBags().map(bag => bag.bag_name)\n`, '\n');
+		submit(`tests.addTiddler.intro()`, `\n\nStart test - addTiddler`);
+	}, 2000);
 }
-*/
 
+// Show our name / version
+intro();
+
+// Remove the existing database.sqlite if present
+//  and starup TiddlyWiki / Multi Wiki Server / REPL
+removeStoreDb().then(() => startup());
